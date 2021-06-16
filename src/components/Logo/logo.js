d905bd5958398d7b22b1883d04537ab410e91c6c
CoderJoe82 <coderjoe82@gmail.com>
2021-06-16 01:00:47 -0400

    forgot to add alt property to image

--- src/components/Logo/logo.js
+++ src/components/Logo/logo.js
@@ -5,7 +5,7 @@ class Logo extends Component {
     render() {
         return (
             <div id = "logo">
-                <img src = {process.env.PUBLIC_URL + '/vinyl.viper.logo.png'} id = "logoImage"/>
+                <img alt = "logo" src = {process.env.PUBLIC_URL + '/vinyl.viper.logo.png'} id = "logoImage"/>
                 <div id = "vinylV">V</div>
                 <div id = "vinylI">I</div>
                 <div id = "vinylN">N</div>
