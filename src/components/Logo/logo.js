8dc8eff3fc8eb214f4797c286ccf737202a7f3aa
CoderJoe82 <coderjoe82@gmail.com>
2021-06-16 01:00:17 -0400

    iper logo image

--- src/components/Logo/logo.js
+++ src/components/Logo/logo.js
@@ -6,6 +6,16 @@ class Logo extends Component {
         return (
             <div id = "logo">
                 <img src = {process.env.PUBLIC_URL + '/vinyl.viper.logo.png'} id = "logoImage"/>
+                <div id = "vinylV">V</div>
+                <div id = "vinylI">I</div>
+                <div id = "vinylN">N</div>
+                <div id = "vinylY">Y</div>
+                <div id = "vinylL">L</div>
+                <div id = "viperV">V</div>
+                <div id = "viperI">I</div>
+                <div id = "viperP">P</div>
+                <div id = "viperE">E</div>
+                <div id = "viperR">R</div>
             </div>
         )
     }
